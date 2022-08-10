39eb56e65bb56af0b47f50b42b05f5cdbdbaeb15
Mikołaj Chojnacki <mikolaj.chojnacki@wp.pl>
2022-08-10 12:28:53 +0200

    Refactor script.js

--- js/script.js
+++ js/script.js
@@ -1,50 +1,54 @@
-function getMoveName(argMoveId) {
+{
+    const getMoveName = function (argMoveId) {
 
-    if (argMoveId == 1) {
-        return 'kamień';
-    } else if (argMoveId == 2) {
-        return `papier`
-    } else if (argMoveId == 3) {
-        return `nożyce`
+        if (argMoveId == 1) {
+            return 'kamień';
+        } else if (argMoveId == 2) {
+            return `papier`
+        } else if (argMoveId == 3) {
+            return `nożyce`
+        }
+
+        printMessage('Nie znam ruchu o id ' + argMoveId + '.');
+        return 'nieznany ruch';
     }
 
-    printMessage('Nie znam ruchu o id ' + argMoveId + '.');
-    return 'nieznany ruch';
-}
+    const displayResult = function (argComputerMove, argPlayerMove) {
 
-function displayResult(argComputerMove, argPlayerMove) {
-    if (argPlayerMove == argComputerMove) {
-        printMessage(`remis`)
-    } else if (argPlayerMove == `kamień` && argComputerMove == `papier`) {
-        printMessage(`Wygrywa komputer`)
-    } else if (argPlayerMove == `kamień` && argComputerMove == `nożyce`) {
-        printMessage(`Ty wygrywasz`)
-    } else if (argPlayerMove == `papier` && argComputerMove == `kamień`) {
-        printMessage(`Ty wygrywasz`)
-    } else if (argPlayerMove == `papier` && argComputerMove == `nożyce`) {
-        printMessage(`Wygrywa komputer`)
-    } else if (argPlayerMove == `nożyce` && argComputerMove == `papier`) {
-        printMessage(`Ty wygrywasz`)
-    } else if (argPlayerMove == `nożyce` && argComputerMove == `kamień`) {
-        printMessage(`Wygrywa komputer`)
-    } else {
-        printMessage(`nieznany ruch`)
+        if (argPlayerMove == argComputerMove) {
+            printMessage(`remis`)
+        } else if (argPlayerMove == `kamień` && argComputerMove == `papier`) {
+            printMessage(`Wygrywa komputer`)
+        } else if (argPlayerMove == `kamień` && argComputerMove == `nożyce`) {
+            printMessage(`Ty wygrywasz`)
+        } else if (argPlayerMove == `papier` && argComputerMove == `kamień`) {
+            printMessage(`Ty wygrywasz`)
+        } else if (argPlayerMove == `papier` && argComputerMove == `nożyce`) {
+            printMessage(`Wygrywa komputer`)
+        } else if (argPlayerMove == `nożyce` && argComputerMove == `papier`) {
+            printMessage(`Ty wygrywasz`)
+        } else if (argPlayerMove == `nożyce` && argComputerMove == `kamień`) {
+            printMessage(`Wygrywa komputer`)
+        } else {
+            printMessage(`nieznany ruch`)
+        }
+
     }
+
+    const playGame = function (argPlayerMove) {
+        const randomNumber = Math.floor(Math.random() * 3 + 1);
+        const computerMove = getMoveName(randomNumber);
+        const playerMove = getMoveName(argPlayerMove);
+
+        console.log('Wylosowana liczba to: ' + computerMove);
+        console.log('Gracz wpisał: ' + playerMove);
+
+        clearMessages();
+        displayResult(computerMove, playerMove);
+    }
+
+    document.getElementById(`play-rock`).addEventListener(`click`, function () { playGame(1) });
+    document.getElementById(`play-paper`).addEventListener(`click`, function () { playGame(2) });
+    document.getElementById(`play-scissors`).addEventListener(`click`, function () { playGame(3) });
+
 }
-
-function playGame(argPlayerMove) {
-    let randomNumber = Math.floor(Math.random() * 3 + 1);
-    let computerMove = getMoveName(randomNumber);
-    let playerMove = getMoveName(argPlayerMove);
-
-    console.log('Wylosowana liczba to: ' + computerMove);
-    console.log('Gracz wpisał: ' + playerMove);
-
-    clearMessages();
-    displayResult(computerMove, playerMove);
-}
-
-document.getElementById(`play-rock`).addEventListener(`click`, function () { playGame(1) });
-document.getElementById(`play-paper`).addEventListener(`click`, function () { playGame(2) });
-document.getElementById(`play-scissors`).addEventListener(`click`, function () { playGame(3) });
-
